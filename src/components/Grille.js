7beb42cad3195ad3a4047f6e87a6449178e47c07
ExcaliburSims <ximeonzilu@gmail.com>
2023-03-25 14:52:05 +0100

    feat: automatique move

--- src/components/Grille.js
+++ src/components/Grille.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/grille.css";
 
-const BOARD_SIZE = 10;
+const BOARD_SIZE = 6;
 
 class LinkedListNode {
   constructor(value) {
@@ -34,7 +34,59 @@ function GridPage() {
   const [board, setBoard] = useState(createBoard(BOARD_SIZE));
   const [car, setCar] = useState(new LinkedList(1));
   const [carCells, setCarCells] = useState(new Set([1]));
+  /*   function handleKeyDown(event) {
+    const newCarCells = new Set(carCells);
 
+    // Move the car up
+    if (event.key === "ArrowUp") {
+      const newHead = car.head.value - BOARD_SIZE;
+      if (newHead >= 1) {
+        newCarCells.delete(car.tail.value);
+        const newHeadNode = new LinkedListNode(newHead);
+        newHeadNode.next = car.head;
+        setCar(new LinkedList(newHead));
+        newCarCells.add(newHead);
+        setCarCells(newCarCells);
+      }
+    }
+    // Move the car down
+    else if (event.key === "ArrowDown") {
+      const newHead = car.head.value + BOARD_SIZE;
+      if (newHead <= BOARD_SIZE * BOARD_SIZE) {
+        newCarCells.delete(car.tail.value);
+        const newHeadNode = new LinkedListNode(newHead);
+        newHeadNode.next = car.head;
+        setCar(new LinkedList(newHead));
+        newCarCells.add(newHead);
+        setCarCells(newCarCells);
+      }
+    }
+  } */
+
+  const moveCar = () => {
+    const newCarCells = new Set(carCells);
+    const newHead = car.head.value + BOARD_SIZE;
+
+    if (newHead <= BOARD_SIZE * BOARD_SIZE) {
+      newCarCells.delete(car.tail.value);
+      const newHeadNode = new LinkedListNode(newHead);
+      newHeadNode.next = car.head;
+      setCar(new LinkedList(newHead));
+      newCarCells.add(newHead);
+      setCarCells(newCarCells);
+    } else {
+      const newCar = new LinkedList(1);
+      setCar(newCar);
+      setCarCells(new Set([1]));
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(moveCar, 500);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [carCells]);
   return (
     <div className="board">
       {board.map((row, rowIdx) => (
