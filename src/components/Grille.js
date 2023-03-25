f3597ac346c041490ded7a0ee78e0fe06b614b6d
ExcaliburSims <ximeonzilu@gmail.com>
2023-03-25 16:52:40 +0100

    feat: add second car move bottom to top

--- src/components/Grille.js
+++ src/components/Grille.js
@@ -34,6 +34,8 @@ function GridPage() {
   const [board, setBoard] = useState(createBoard(BOARD_SIZE));
   const [car, setCar] = useState(new LinkedList(1));
   const [carCells, setCarCells] = useState(new Set([1]));
+  const [car2, setCar2] = useState(new LinkedList(312));
+  const [car2Cells, setCar2Cells] = useState(new Set([312]));
 
   const moveCar = () => {
     const newCarCells = new Set(carCells);
@@ -52,13 +54,32 @@ function GridPage() {
       setCarCells(new Set([1]));
     }
   };
+  const moveCarUp = () => {
+    const newCar2Cells = new Set(car2Cells);
+    const newHead2 = car2.head.value - BOARD_SIZE;
+
+    if (newHead2 > 0) {
+      newCar2Cells.delete(car2.tail.value);
+      const newHeadNode2 = new LinkedListNode(newHead2);
+      newHeadNode2.next = car2.head;
+      setCar2(new LinkedList(newHead2));
+      newCar2Cells.add(newHead2);
+      setCar2Cells(newCar2Cells);
+    } else {
+      const newCar2 = new LinkedList(312);
+      setCar2(newCar2);
+      setCar2Cells(new Set([312]));
+    }
+  };
 
   useEffect(() => {
-    const intervalId = setInterval(moveCar, 500);
+    const intervalId = setInterval(moveCar, 50);
+    const intervalId2 = setInterval(moveCarUp, 50);
     return () => {
       clearInterval(intervalId);
+      clearInterval(intervalId2);
     };
-  }, [carCells]);
+  }, [carCells, car2Cells]);
   return (
     <div className="board">
       {board.map((row, rowIdx) => (
@@ -67,7 +88,9 @@ function GridPage() {
             return (
               <div
                 key={cellIdx}
-                className={`cell ${carCells.has(cellValue) ? "car-cell" : ""}`}
+                className={`cell ${carCells.has(cellValue) ? "car-cell" : ""} ${
+                  car2Cells.has(cellValue) ? "car-cell2" : ""
+                }`}
               ></div>
             );
           })}
